58b33dc15222dfbbb13c3238e54a51f3830a1355
imqvictor <imavictor9@gmail.com>
2026-06-03 02:30:08 +0300

    updated index and script files

--- script.js
+++ script.js
@@ -13,8 +13,13 @@ let button1 = document.querySelector('#btn1');
 let button2 = document.querySelector('#btn2');
 let button3 = document.querySelector('#btn3');
 let text = document.querySelector('#txt');
+let monSterStarts = document.querySelector('#monSterStarts');
+let monsterName = document.querySelector('#monsterName');
+let monsterHealthText = document.querySelector('#monsterHealth');
 
-
+xpText.innerHTML = xp;
+healthText.innerHTML = health;
+goldText.innerHTML = gold;
 
 const weapons = [
     { name: 'stick', power: 5 },
@@ -54,7 +59,7 @@ const locations = [
     {
         name: "fight",
         "button text": ["Attack", "Dodge", "Run"],
-        "buton functions": [attack, doge, goTown],
+        "button functions": [attack, dodge, goTown],
         text: "You are fighting a monster."
     },
     {
@@ -75,7 +80,12 @@ const locations = [
         "button functions": [reStart, reStart, reStart],
         text: "You defeat the dragon!. YOU WIN THE GAME!"
     },
-
+    {
+        name: "easter egg",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [reStart, reStart, reStart],
+        text: "You found the easter egg! YOU WIN THE GAME!"
+    }
 ]
 
 //initalize buttons
@@ -84,6 +94,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+    monSterStarts.style.display = "none";
 
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
@@ -108,29 +119,169 @@ function goCave() {
     update(locations[2]);
 }
 
-function fightDragon() {
-    alert('Hello');
-}
-
 function buyHealth() {
-    healthText.innerHTML = health;
-}
-function buyWeapon() {
-    goldText.innerHTML = gold;
-}
-function fightSlime() {
-    xpText.innerHTML = xp;
-}
-function fightBeast() {
-    alert('Hello');
-}
-function attack() {
-    alert('Hello');
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+        goldText.innerHTML = gold;
+        healthText.innerHTML = health;
+    } else {
+        text.innerHTML = "You don't have enough gold to buy health.";
+    }
 }
 
-function doge() {
-    alert('Hello');
+function buyWeapon() {
+    if (currentWeapon < weapons.length - 1) {
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeapon++;
+            goldText.innerHTML = gold;
+
+            let newWeapon = weapons[currentWeapon].name + ".";
+            text.innerHTML = "You now have a " + newWeapon;
+            inventory.push(newWeapon);
+            text.innerHTML += " In your inventory you have: " + inventory;
+        } else {
+            text.innerHTML = "You don't have enough gold to buy a weapon.";
+        }
+    } else {
+        text.innerHTML = "You already have the most powerful weapon!";
+        button2.innerHTML = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
+    }
 }
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold += 15;
+        goldText.innerHTML = gold;
+        let currentWeapon = inventory.shift();
+        text.innerHTML = "You sold your " + currentWeapon + ".";
+        text.innerHTML += " In your inventory you have: " + inventory;
+    } else {
+        text.innerHTML = "Don't sell your only weapon!";
+    }
+}
+
+function fightSlime() {
+    fighting = 0;
+    goFight();
+}
+
+function fightBeast() {
+    fighting = 1;
+    goFight();
+}
+
+function fightDragon() {
+    fighting = 2;
+    goFight();
+}
+
+function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monSterStarts.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
+}
+
+function attack() {
+    text.innerHTML = "The " + monsters[fighting].name + " attacks. ";
+    text.innerHTML += "You attack it with your " + weapons[currentWeapon].name + ".";
+    health -= getMonstersAttackValue(monsters[fighting].level);
+    if (isMonsterHit()) {
+        monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    } else {
+        text.innerText += " You missed.";
+    }
+    healthText.innerHTML = health;
+    monsterHealthText.innerHTML = monsterHealth;
+    if (health <= 0) {
+        lose();
+    } else if (monsterHealth <= 0) {
+        if (fighting === 2) {
+            winGame();
+        } else {
+            defeatMonster();
+        }
+    }
+    if (Math.random() <= .1 && inventory.length > 1) {
+        text.innerHTML += " Your " + inventory.pop() + " breaks.";
+        currentWeapon--;
+    }
+}
+
+function getMonstersAttackValue(level) {
+    const hit = (level * 5) - (Math.floor(Math.random() * xp));
+    console.log(hit);
+    return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+    return Math.random() > .2 || health < 20;
+}
+
+function dodge() {
+    text.innerHTML = "You dodge the attack from the " + monsters[fighting].name + ".";
+}
+
+function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerHTML = gold;
+    xpText.innerHTML = xp;
+    update(locations[4]);
+}
+
+function lose() {
+    update(locations[5]);
+}
+
+function winGame() {
+    update(locations[6]);
+}
+
 function reStart() {
-    alert('Hello');
+    xp = 10;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    inventory = ["stick"];
+    xpText.innerHTML = xp;
+    healthText.innerHTML = health;
+    goldText.innerHTML = gold;
+    goTown();
+}
+
+function easterEgg() {
+    update(locations[7]);
+}
+
+function pickTwo() {
+    pick(2);
+}
+
+function pickEight() {
+    pick(8);
+}
+
+function pick() {
+    const numbers = [];
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 10) + 1);
+    }
+    text.innerHTML = "You picked " + guess + ". Here are the random numbers:\n";
+    for (let i = 0; i < numbers.length; i++) {
+        text.innerHTML += numbers[i] + " ";
+    }
+    if (numbers.includes(guess)) {
+        text.innerHTML += "Right you win 20 gold!";
+        gold += 20;
+        goldText.innerHTML = gold;
+    } else {
+        text.innerHTML += "Wrong! you lose 10 health!";
+        health -= 10;
+        healthText.innerHTML = health;
+    }
+
 }
